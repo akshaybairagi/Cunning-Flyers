210e8e56abb09ec9876e22cfd7c3432573d1198b
Akshay Bairagi <akshaybairagi@3rimgames.com>
2016-08-11 12:34:21 +0530

    Camera Controller
    Camera Controller script added to follow the player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    Transform player;
+
+    float offsetY;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject player_go = GameObject.FindGameObjectWithTag("Player");
+
+        if (player_go == null)
+        {
+            Debug.LogError("Couldn't find an object with tag 'Player'!");
+            return;
+        }
+
+        player = player_go.transform;
+
+        offsetY = transform.position.y - player.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector3 pos = transform.position;
+            pos.y = player.position.y + offsetY;
+            transform.position = pos;
+        }
+    }
+}
