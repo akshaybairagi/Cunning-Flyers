36dcafbb120f7a5e56968b7445df4faa68b78e76
Akshay Bairagi <akshaybairagi@3rimgames.com>
2016-08-29 13:27:03 +0530

    Orthographic camera size changed from 12 to 10

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.Advertisements;
 using System.Collections;
 
 
@@ -7,27 +6,7 @@ public class AdManager : MonoBehaviour {
 
 	public void ShowAds()
     {
-        if (Advertisement.IsReady())
-        {
-            Advertisement.Show("rewardedVideo", new ShowOptions() { resultCallback = HandleAdCallback});
-        }
+        GameController.control.ContinuePlay();
     }
-
-    private void HandleAdCallback(ShowResult result)
-    {
-        switch (result)
-        {
-            case ShowResult.Finished:
-                GameController.control.ContinuePlay();
-                break;
-
-            case ShowResult.Skipped:
-                Debug.Log("Ad Skipped");
-                break;
-
-            case ShowResult.Failed:
-                Debug.Log("Ad Failed");
-                break;
-        }
-    }
+        
 }
