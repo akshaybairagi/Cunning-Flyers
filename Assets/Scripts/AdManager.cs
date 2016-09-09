8dc1e61de69a2046afe2ddd8aecac621ce801b07
Akshay Bairagi <akshaybairagi@3rimgames.com>
2016-09-09 10:00:00 +0530

    Device Filter
    changed to ARMv7

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -1,33 +1,11 @@
 ﻿using UnityEngine;
-using UnityEngine.Advertisements;
-using System.Collections;
 
 public class AdManager : MonoBehaviour
 {
 
     public void ShowAds()
     {
-        if (Advertisement.IsReady())
-        {
-            Advertisement.Show("rewardedVideo", new ShowOptions() { resultCallback = HandleAdCallback });
-        }
+        GameController.control.ContinuePlay();
     }
-
-    private void HandleAdCallback(ShowResult result)
-    {
-        switch (result)
-        {
-            case ShowResult.Finished:
-                GameController.control.ContinuePlay();
-                break;
-
-            case ShowResult.Skipped:
-                Debug.Log("Ad Skipped");
-                break;
-
-            case ShowResult.Failed:
-                Debug.Log("Ad Failed");
-                break;
-        }
-    }
+    
 }
