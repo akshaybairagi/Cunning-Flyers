a6d49e345ce6ae6a675a761b9bdbf65163ad6893
Akshay Bairagi <akshaybairagi@3rimgames.com>
2016-09-11 00:54:15 +0530

    Pause working

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -3,24 +3,22 @@ using System.Collections;
 
 public class PauseManager : MonoBehaviour {
 
-    public static PauseManager instance;
-
     public GameObject pausePanel;
 
-    public void PauseGame()
-    {
-        pausePanel.SetActive(true);
-        Time.timeScale = 0;
-    }
-
     void OnApplicationPause(bool pauseStatus)
-    {        
+    {
         if (pauseStatus == true)
         {
             PauseGame();
         }
     }
 
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+    
     public void UnPauseGame()
     {
         pausePanel.SetActive(false);
