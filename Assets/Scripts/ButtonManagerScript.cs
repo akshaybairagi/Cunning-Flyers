11a620a1d1a429bcf460af39ca0e2ea1c21c1c7a
Akshay Bairagi <akshaybairagi@3rimgames.com>
2016-09-16 22:42:13 +0530

    Gameobject  order changed in gamescene.

--- Assets/Scripts/ButtonManagerScript.cs
+++ Assets/Scripts/ButtonManagerScript.cs
@@ -2,19 +2,22 @@
 
 public class ButtonManagerScript : MonoBehaviour {
 
+    // show leaderboard UI
     public void ShowLeaderboardUI()
     {
-        // show leaderboard UI
         if (GameController.instance.IsUserAuthenticated == true)
             Social.ShowLeaderboardUI();
     }
 
+    // show Settings UI Panel
     public void ShowSettingsUI()
     {
         GameController.instance.SetCurrentState(GameState.Settings);
         UIManager.instance.MenuController(GameState.Settings);
     }
 
+
+    //Exit Settings Panel
     public void ExitSettingsUI()
     {
         GameController.instance.SetCurrentState(GameState.Gameover);
